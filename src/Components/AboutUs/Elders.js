85b22029bf2ee7011833aae4a076d1a02199300a
Jobal <jbaldwije@gmail.com>
2020-09-16 20:13:12 -0400

    elders

--- src/Components/AboutUs/Elders.js
+++ src/Components/AboutUs/Elders.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import pastor from '../../Images/pastor.jpg';
+import musician from '../../Images/musician.jpg';
+import youth from '../../Images/youngpastor.jpg';
+import secretary from '../../Images/pastor.jpg';
+import assistant from '../../Images/pastor.jpg';
 import './Elders.css';
 
 export default class Purpose extends Component {
@@ -11,32 +16,22 @@ export default class Purpose extends Component {
             <h1>Elders</h1>
           </div>
         </div>
-        {/* <div className='Elders__content'>
-          <div className='PCc'>
-            <div className='PCcTitle'>
-              <h2>God Comes First</h2>
+        <div className='Elders__content'>
+          <div className='ECc'>
+            <div className='ECcProfile'>
+              <img className='ECcPImg ECPastor' src={pastor} alt='pastor'/>
+              <p className='ECcPTitle'>Clint Hopson, Senior Pastor</p>
             </div>
-            <div className='PCcContent'>
-              <p>Every sermon, teaching, event, and small group is made to put God first. We want to celebrate God's goodness and honor him in everythign we do because Jesus died on the cross for our sins and rose from the grave on the third day.</p>
+            <div className='ECcProfile'>
+              <img className='ECcPImg ECYouth' src={youth} alt='youth'/>
+              <p className='ECcPTitle'>Brock Humbucker, Young Adults Pastor</p>
             </div>
-          </div>
-          <div className='PCc'>
-            <div className='PCcTitle'>
-              <h2>Fellowship with One Another</h2>
-            </div>
-            <div className='PCcContent'>
-              <p>We are a loving church. Those who walk through our doors aren’t just people—they’re family. We take care of each other and pray for each other. We want to help each other grow in Christ and use our gifts for Christ’s kingdom. Whether it’s at church, in our homes, or out in the community, we consider fellowship, prayer, and group Bible study an integral part of church life.</p>
+            <div className='ECcProfile'>
+              <img className='ECcPImg ECMusc' src={musician} alt='musician'/>
+              <p className='ECcPTitle'>Grizzley McDuff, Worship Pastor</p>
             </div>
           </div>
-          <div className='PCc'>
-            <div className='PCcTitle'>
-              <h2>Share the Gospel</h2>
-            </div>
-            <div className='PCcContent'>
-              <p>Our Great Commission Mission is to get the Gospel in the ears of the world, invite them to repent, believe, and call upon the name of the Lord. These new believers are baptized and taugh to obey Christ.</p>
-            </div>
-          </div>
-        </div> */}
+        </div>
       </div>
     )
   }
